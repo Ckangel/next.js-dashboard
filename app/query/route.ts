717657a78398b8a4797b7166c2491be3097ce485
Ckangel <ckajeigbe1@gmail.com>
2025-11-13 23:54:11 +0000

    dashboard shows chart

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,42 +1,38 @@
 import postgres from 'postgres';
-
-import type { NextRequest } from 'next/server'
- 
-export async function GET(request: NextRequest) {
-  const url = request.nextUrl
-}
+import { NextRequest, NextResponse } from 'next/server';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function listInvoices() {
- 	const data = await sql`
+  const data = await sql`
      SELECT invoices.amount, customers.name
      FROM invoices
      JOIN customers ON invoices.customer_id = customers.id
      WHERE invoices.amount = 666;
    `;
 
- 	return data;
-} 
+  return data;
+}
 
-export async function GET() {
+export async function GET(request: NextRequest) {
   try {
-    return Response.json(await listInvoices());
+    const data = await listInvoices();
+    return NextResponse.json(data);
   } catch (error) {
+    console.error(error);
+    return new Response('Internal Server Error', { status: 500 });
   }
 }
 
-export async function GET(request: Request) {}
- 
 export async function HEAD(request: Request) {}
- 
+
 export async function POST(request: Request) {}
- 
+
 export async function PUT(request: Request) {}
- 
+
 export async function DELETE(request: Request) {}
- 
+
 export async function PATCH(request: Request) {}
- 
+
 // If `OPTIONS` is not defined, Next.js will automatically implement `OPTIONS` and set the appropriate Response `Allow` header depending on the other methods defined in the Route Handler.
 export async function OPTIONS(request: Request) {}
