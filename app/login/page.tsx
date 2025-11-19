2d694d57e914ae32dd7b59c3424bb07d5c299d4b
Ckangel <ckajeigbe1@gmail.com>
2025-11-19 06:59:52 +0000

    add middleware to root

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
-import { Suspense } from 'react';
- 
+import { Metadata } from 'next';
+
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
@@ -11,10 +11,12 @@ export default function LoginPage() {
             <AcmeLogo />
           </div>
         </div>
-        <Suspense>
-          <LoginForm />
-        </Suspense>
+        <LoginForm />
       </div>
     </main>
   );
 }
+
+export const metadata: Metadata = {
+  title: 'Login',
+};
