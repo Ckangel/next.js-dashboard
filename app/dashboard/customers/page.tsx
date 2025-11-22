d2824af773864550600194d152e3101bb9158a42
Ckangel <ckajeigbe1@gmail.com>
2025-11-22 15:37:32 +0000

    move files  dashboard/invoice to ui/invoice & customer/page tsx updates

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,25 @@
- import { Metadata } from 'next';
- 
+import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
 export const metadata: Metadata = {
-  title: 'Customer',
+  title: 'Customers',
 };
-export default function Page() {
-  return <p>Customer Page</p>;
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
 }
